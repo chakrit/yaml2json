3270b61bbb121253d88cb08f8dbfe67d83bcbf4f
Chakrit Wichian <chakrit@oozou.com>
2012-05-24 01:12:40 +0700

    Implement a simple JSON emitting to test things out.

--- src/yaml2json.c
+++ src/yaml2json.c
@@ -6,10 +6,11 @@ int
 main() {
   json_object *valueStr = json_object_new_string("World");
   json_object *obj = json_object_new_object();
+  const char *jsonStr = NULL;
 
-  json_object_add(obj, "Hello", valueStr);
-  char *jsonStr = json_object_get_string(obj);
+  json_object_object_add(obj, "Hello", valueStr);
+  jsonStr = json_object_get_string(obj);
+
   printf("%s", jsonStr);
-
   return 0;
 }
